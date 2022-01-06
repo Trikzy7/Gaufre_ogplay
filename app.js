321571449b326dbcc2c59000fe7065e66e4ed693
Trikzy7 <theoplebani2002@icloud.com>
2022-01-06 14:13:34 +0100

    Add form and footer

--- app.js
+++ app.js
@@ -5,18 +5,32 @@
 
 const container_form = document.querySelector('.container_form');
 const btn_form_game = document.querySelector('.btn_form_game');
+const input_pseudo = document.querySelector('#input_pseudo');
+const input_email = document.querySelector('#input_email');
+const input_data_policy = document.querySelector('#input_data_policy');
+
+
+let roue = document.querySelector(".img_wheel")
+let button = document.querySelector(".btn_wheel")
+const info_game_gain = document.querySelector('.info_game_gain');
 
 
 
+//-------- FORM
 btn_form_game.addEventListener('click', ()=> {
-    container_form.classList.add('display_none');
+
+	if (input_pseudo.value != '' && input_email.value != '' && input_data_policy.checked == true) {
+		container_form.classList.add('display_none');
+	}
+  
 })
 
 
 
 
-let roue = document.querySelector(".img_wheel")
-let button = document.querySelector(".btn_wheel")
+
+
+//-------- WHEEL
 let deg = 0
 let speed = 0
 button.addEventListener("click", ()=>{
@@ -25,12 +39,21 @@ button.addEventListener("click", ()=>{
 
 setInterval(()=>{
 	deg+= speed
-	console.log(speed)
 	roue.style.transform = 'rotate('+deg+'deg)';
 	if(speed >0.01)
 		speed-=speed/100
 	else speed=0
 }, 10)
+
+
+
+button.addEventListener('click', ()=> {
+
+	setTimeout(() => {
+		button.classList.add('display_none');
+		info_game_gain.style.display = 'unset';
+	}, 1000);
+})
 
 
 
